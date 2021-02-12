8ffe8daa0ee9969fdb5807a42fcac89751b21ac8
sajibmahamud <sajibmahamud3@gmail.com>
2021-02-12 22:04:05 +0600

    Quatable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { MenubarComponent } from './menubar/menubar.component';
+import { FavoriteAuthorComponent } from './favorite-author/favorite-author.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthorComponent } from './author/author.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"author",component:AuthorComponent},
+  {path:"favorite-author",component:FavoriteAuthorComponent},
+  {path:"menubar",component:MenubarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { FavoriteAuthorComponent } from './favorite-author/favorite-author.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorComponent } from './author/author.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import { ReactiveFormsModule } from '@angular/forms';;
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorComponent,
+    FavoriteAuthorComponent,
+    MenubarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
